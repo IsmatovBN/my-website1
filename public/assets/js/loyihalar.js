12569a7c1b4013f1ed0a29dd3dee6e7dd3094936
IsmatovBN <ismatovbaxtishodm6s@gmail.com>
2025-11-09 07:29:13 -0800

    Delete public directory

--- public/assets/js/loyihalar.js
+++
@@ -1,8 +0,0 @@
-async function loadProjects() {
-  const res = await fetch('http://localhost:3000/posts')
-  const projects = await res.json();
-  document.getElementById('projectList').innerHTML = projects.map(
-    p => `<div><h3>${p.title}</h3><p>${p.description}</p></div>`
-  ).join('');
-}
-loadProjects();
